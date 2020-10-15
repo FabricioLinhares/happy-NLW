3a15af7527d0ab230a23ead5c0b1d6afbe04021e
Fabricio Linhares Rodrigues <fabriciorodrigues1231@outlook.com>
2020-10-15 13:22:08 -0300

    map orphanages page

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,22 @@
+const map = L.map('mapid').setView([-27.2198158,-49.6477953], 16)
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
+
+const icon = L.icon({
+    iconUrl: './public/images/map-marker.svg',
+    iconSize: [58,68],
+    iconAnchor: [29,68],
+    popupAnchor: [170,2]
+})
+
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"></a>')
+
+L
+.marker([-27.2198158,-49.6477953], {icon})
+.addTo(map)
+.bindPopup(popup)
