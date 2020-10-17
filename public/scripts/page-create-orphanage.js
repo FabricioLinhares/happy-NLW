e71ed0acaefb7a1e77f119eb1b9ffb1f4bac6407
Fabricio Linhares Rodrigues <fabriciorodrigues1231@outlook.com>
2020-10-17 14:19:29 -0300

    get orphanages info from backend

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -3,7 +3,7 @@ const map = L.map('mapid').setView([-27.2198158,-49.6477953], 16)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
 
 const icon = L.icon({
-    iconUrl: './public/images/map-marker.svg',
+    iconUrl: '/images/map-marker.svg',
     iconSize: [58,68],
     iconAnchor: [29,68],
     popupAnchor: [170,2]
--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -3,20 +3,34 @@ const map = L.map('mapid').setView([-27.2198158,-49.6477953], 16)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
 
 const icon = L.icon({
-    iconUrl: './public/images/map-marker.svg',
+    iconUrl: '/images/map-marker.svg',
     iconSize: [58,68],
     iconAnchor: [29,68],
     popupAnchor: [170,2]
 })
 
-const popup = L.popup({
-    closeButton: false,
-    className: 'map-popup',
-    minWidth: 240,
-    minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"></a>')
+function addMarker({ id, name, lat, lng }) {
+    const popup = L.popup({
+        closeButton: false,
+        className: 'map-popup',
+        minWidth: 240,
+        minHeight: 240
+    }).setContent(`${name} <a href="orphanage?id=${id}"> <img src="/images/arrow-white.svg"></a>`)
+    
+    L
+    .marker([lat,lng], {icon})
+    .addTo(map)
+    .bindPopup(popup)
+}
 
-L
-.marker([-27.2198158,-49.6477953], {icon})
-.addTo(map)
-.bindPopup(popup)
+const orphanagesSpan = document.querySelectorAll('#orphanages span')
+orphanagesSpan.forEach(span => {
+    const orphanage = {
+        id: span.dataset.id,
+        name: span.dataset.name,
+        lat: span.dataset.lat,
+        lng: span.dataset.lng,
+    }
+
+    addMarker(orphanage)
+})
