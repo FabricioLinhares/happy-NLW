acb60dca00c9d805958270a37cc89d0219f0e1bb
Fabricio Linhares Rodrigues <fabriciorodrigues1231@outlook.com>
2020-10-17 13:56:48 -0300

    backend init

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -14,7 +14,7 @@ const popup = L.popup({
     className: 'map-popup',
     minWidth: 240,
     minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"></a>')
+}).setContent('Lar das meninas <a href="orphanage" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"></a>')
 
 L
 .marker([-27.2198158,-49.6477953], {icon})
